ea73fe3a540ec1636624390e395da4e84eb628de
jamal-pb95 <jamal.pb95@gmail.com>
2018-06-29 17:10:45 +0600

    finished 2nd tutorial

---
+++ methods.js
@@ -0,0 +1,15 @@
+const methods = {};
+
+methods.updateServer = () => {
+	console.log('Important Function!');
+}
+
+methods.eatCookies = () => {
+	console.log('Chocolate Cookies!');
+}
+
+methods.node = () => {
+	console.log('Node.js is Awesome!');
+}
+
+exports.data = methods;
--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-console.log('Hello World!');
-
-for(let i = 0; i < 10; i++) {
-	console.log(i);
-}
+let response = require('./methods.js');
+console.log(response);
