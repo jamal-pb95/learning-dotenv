4f86e1e8f7a360d2b5625fbe3d529fca0f00d0a6
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-01 10:23:30 +0600

    development server ready for work

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('./css', express.static(__dirname + './assets'));
+app.get('/', (req, res) => {
+	res.sendFile('index.html');
+});
+
+app.listen(3000, () => console.log('Server started on port 3000...'));
