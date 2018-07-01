42dacb779d271806edfbee19a20fb42bffaf6acc
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-01 12:12:01 +0600

    start routing on express

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+// const fs = require('fs');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 const app = express();
+const router = express.Router();
 
-app.use(bodyParser());
-app.use(cookieParser());
+// app.use(bodyParser());
+// app.use(cookieParser());
 
 app.use('./css', express.static(__dirname + './assets'));
 
-// Cookie
-app.get('/', (req, res) => {
-	res.cookie('myFirstCookie', 'looks Good');
-	res.end('Wow');
+app.use('/', router);
+
+app.get('/first-router', (req, res) => {
+	res.end('What\'s going on...');
 });
 
-// Remove cookie
-app.get('/remove-cookie', (req, res) => {
-	res.clearCookie('myFirstCookie');
-	res.end('Wow');
+app.get('/second-router', (req, res) => {
+	res.end('What\'s going on...again and again...');
 });
+// Cookie
+// app.get('/', (req, res) => {
+// 	res.cookie('myFirstCookie', 'looks Good');
+// 	res.end('Wow again without server restart. dddd');
+// });
+
+// Remove cookie
+// app.get('/remove-cookie', (req, res) => {
+// 	res.clearCookie('myFirstCookie');
+// 	res.end('Wow');
+// });
 
 // app.get('/', (req, res) => {
 // 	res.sendFile('index.html', {root: path.join(__dirname, '/')});
