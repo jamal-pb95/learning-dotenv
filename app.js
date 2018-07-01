19c28748d4f978134e87fc3738dbce258289b700
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-01 10:35:32 +0600

    learnt about GET method

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use('./css', express.static(__dirname + './assets'));
 app.get('/', (req, res) => {
-	res.sendFile('index.html', {root: path.join(__dirname, '/')});
+	// res.sendFile('index.html', {root: path.join(__dirname, '/')});
+	let greet = `Hello ${req.query.firstName}!`;
+	res.end(greet);
 });
 
 app.listen(3000, () => console.log('Server started on port 3000...'));
